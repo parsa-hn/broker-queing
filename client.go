4f64ea24b4b22bf9a2fe1265a40944d47ae2307b
saber <saber.en@yahoo.com>
2020-11-06 19:29:09 +0330

    client and server impelementation added

--- client.go
+++ client.go
@@ -1,1 +1,53 @@
+package main
 
+import (
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s ip-addr\n", os.Args[0])
+		os.Exit(1)
+	}
+	brokerAddres := os.Args[1]
+
+	var sleep_delay int
+	task := make([]byte, 512)
+
+	broker, err := net.ResolveTCPAddr("tcp4", brokerAddres)
+	checkError(err)
+	for true {
+		conn, err := net.DialTCP("tcp", nil, broker)
+		checkError(err)
+
+		_, err = conn.Write([]byte("woker ready"))
+		checkError(err)
+
+		_, err = conn.Read(task)
+		if err != nil {
+			continue
+		}
+
+		if string(task) == "close" {
+			fmt.Println("cleint closed")
+			break
+		}
+
+		sleep_delay = rand.Intn(10) + 1
+		fmt.Println("message '%s' recieved and now sleeping for %d seconds", string(task), sleep_delay)
+		time.Sleep(time.Duration(sleep_delay) * time.Second)
+	}
+	os.Exit(0)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
--- server.go
+++ server.go
@@ -1,1 +1,64 @@
+package main
 
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) != 3 {
+		fmt.Println("wrong input format")
+		os.Exit(1)
+	}
+	brokerAddres := os.Args[1]
+	serverType := os.Args[2]
+
+	var input string
+
+	broker, err := net.ResolveTCPAddr("tcp4", brokerAddres)
+	if err != nil {
+		fmt.Println(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+
+	for i := 1; ; i++ {
+
+		fmt.Scanln(&input)
+		if input == "exit" {
+			break
+		}
+
+		if serverType == "sync" {
+			sendMessage(i, broker)
+		}
+		if serverType == "async" {
+			go sendMessage(i, broker)
+		}
+	}
+	os.Exit(0)
+}
+
+func sendMessage(messageNumber int, broker *net.TCPAddr) {
+	ack := make([]byte, 32)
+	conn, err := net.DialTCP("tcp", nil, broker)
+	if err != nil {
+		fmt.Println("connection error when sending message number %d", messageNumber)
+		return
+	}
+
+	_, err = conn.Write([]byte("message"))
+	if err != nil {
+		fmt.Println("connection error when sending message number %d", messageNumber)
+		return
+	}
+
+	_, err = conn.Read(ack)
+	if err != nil {
+		fmt.Println("connection error when sending message number %d", messageNumber)
+		return
+	}
+
+	fmt.Println("ack for '%s' recieved for message numbner %d", string(ack), messageNumber)
+}
